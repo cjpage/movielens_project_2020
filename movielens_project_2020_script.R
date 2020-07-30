e37f03b4c293d136a08a10883ea964fc48678261
Christopher Page <65598163+cjpage@users.noreply.github.com>
2020-07-30 06:10:25 -0400

    Update movielens_project_2020_script.R

--- movielens_project_2020_script.R
+++ movielens_project_2020_script.R
@@ -1,5 +1,50 @@
 ### The purpose of this script is to run the best algorithm (Algorithm 10) against the VALIDATION set
-### The first step is to split VALIDATION into TRAIN and TEST sets
+
+### The first step was to load the necessary libraries from the repository http://cran.us.r-project.org. 
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+
+if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
+
+### The second step entailed loading and wrangling the MovieLens set from http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+l <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+                 col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(movieId),
+                                            title = as.character(title),
+                                            genres = as.character(genres))
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# The third step called for generating the VALIDATION set from 10% of MovieLens data
+
+set.seed(1)
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+validation <- temp %>% 
+      semi_join(edx, by = "movieId") %>%
+      semi_join(edx, by = "userId")
+
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
+### The fourth step was to split VALIDATION into TRAIN and TEST sets
+
 set.seed(755)
 
 test_index <- createDataPartition(y = validation$rating, times = 1, p = 0.2,
@@ -15,7 +60,11 @@ validation_test_set <- validation_test_set %>%
 
 mu <- mean(validation_train_set$rating)
 
-### The second step is to use Algorithm 10 to predict ratings
+### The fifth step was to use Algorithm 10 to predict ratings
+
+lambda03 <- 2.25
+
+lambda05 <- 5.5
 
 movie_reg_avgs <- validation_train_set %>%
   group_by(movieId) %>%
@@ -31,7 +80,7 @@ predicted_ratings_validation <- validation_test_set %>%
   mutate(pred = mu + b_i + b_u) %>%
   pull(pred)
 
-### The third step is to calculate the RMSE
+### The sixth step is to calculate the RMSE
 
 RMSE_VAL <- RMSE(predicted_ratings_validation, validation_test_set$rating)
 
